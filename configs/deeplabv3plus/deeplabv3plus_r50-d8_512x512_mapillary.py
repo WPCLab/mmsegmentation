1c4dd3e1c14ed063f0b2253e34af0b87bc3dccf8
darrenwang <darrenwang@didiglobal.com>
2022-05-10 00:38:40 +0800

    add config

--- configs/deeplabv3plus/deeplabv3plus_r50-d8_512x512_mapillary.py
+++ configs/deeplabv3plus/deeplabv3plus_r50-d8_512x512_mapillary.py
@@ -1,5 +1,5 @@
 _base_ = [
     '../_base_/models/deeplabv3plus_r50-d8.py',
-    '../_base_/datasets/mapillary_512x1024.py', '../_base_/default_runtime.py',
+    '../_base_/datasets/mapillary_512x512.py', '../_base_/default_runtime.py',
     '../_base_/schedules/schedule_80k.py'
 ]
