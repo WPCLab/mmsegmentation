f5c4b0f13be78653e827b8d1f9681b55c1dcd060
darrenwang <darrenwang@didiglobal.com>
2022-05-12 17:24:16 +0800

    rm class weights for baseline

--- configs/_base_/models/deeplabv3plus_r50-d8.py
+++ configs/_base_/models/deeplabv3plus_r50-d8.py
@@ -27,17 +27,7 @@ model = dict(
         norm_cfg=norm_cfg,
         align_corners=False,
         loss_decode=dict(
-            type='CrossEntropyLoss',
-            use_sigmoid=False,
-            loss_weight=1.0,
-            class_weight=[
-                2.831637118444236, 5.056285632204348, 5.359396933797303, 7.141446981306911,
-                7.83548537241383, 7.369427346768545, 5.159373743076446, 4.76376838541227,
-                5.74627485203493, 4.255490074972682, 8.47767089648915, 6.7950642130560635,
-                6.853404414400033, 1.551886637845778, 1.358233603952349, 4.278350321743853,
-                1.137561790141584, 4.282902057952668, 3.7958329552557286, 5.35584437600218,
-                3.001029493800046
-            ])),
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
     auxiliary_head=dict(
         type='FCNHead',
         in_channels=1024,
@@ -50,17 +40,7 @@ model = dict(
         norm_cfg=norm_cfg,
         align_corners=False,
         loss_decode=dict(
-            type='CrossEntropyLoss',
-            use_sigmoid=False,
-            loss_weight=0.4,
-            class_weight=[
-                2.831637118444236, 5.056285632204348, 5.359396933797303, 7.141446981306911,
-                7.83548537241383, 7.369427346768545, 5.159373743076446, 4.76376838541227,
-                5.74627485203493, 4.255490074972682, 8.47767089648915, 6.7950642130560635,
-                6.853404414400033, 1.551886637845778, 1.358233603952349, 4.278350321743853,
-                1.137561790141584, 4.282902057952668, 3.7958329552557286, 5.35584437600218,
-                3.001029493800046
-            ])),
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
     # model training and testing settings
     train_cfg=dict(),
     test_cfg=dict(mode='whole'))
