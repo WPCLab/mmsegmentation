8dab2ea8dd63b82f46673330f6b17bc6e0fc242b
darrenwang <darrenwang@didiglobal.com>
2022-05-13 10:41:27 +0800

    add class weight

--- configs/deeplabv3plus/deeplabv3plus_r50-d8_class_weight_769x769_mapillary.py
+++ configs/deeplabv3plus/deeplabv3plus_r50-d8_class_weight_769x769_mapillary.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/deeplabv3plus_r50-d8_ohem.py',
+    '../_base_/models/deeplabv3plus_r50-d8_class_weight.py',
     '../_base_/datasets/mapillary_769x769.py', '../_base_/default_runtime.py',
     '../_base_/schedules/schedule_160k.py'
 ]
