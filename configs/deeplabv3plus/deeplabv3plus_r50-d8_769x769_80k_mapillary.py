cec5f1c346fefa4c4913e2ef0f70ea69b9f70e0b
darrenwang <darrenwang@didiglobal.com>
2022-05-16 10:50:07 +0800

    use ohem

--- configs/deeplabv3plus/deeplabv3plus_r50-d8_769x769_80k_mapillary.py
+++ configs/deeplabv3plus/deeplabv3plus_r50-d8_769x769_80k_mapillary.py
@@ -1,9 +1,13 @@
 _base_ = [
     '../_base_/models/deeplabv3plus_r50-d8.py',
     '../_base_/datasets/mapillary_769x769.py', '../_base_/default_runtime.py',
-    '../_base_/schedules/schedule_20k.py'
+    '../_base_/schedules/schedule_80k.py'
 ]
 model = dict(
-    decode_head=dict(align_corners=True, num_classes=66),
-    auxiliary_head=dict(align_corners=True, num_classes=66),
-    test_cfg=dict(mode='slide', crop_size=(769, 769), stride=(513, 513)))
+    decode_head=dict(
+        sampler=dict(type='OHEMPixelSampler', min_kept=100000),
+        align_corners=True,
+        num_classes=66),
+    auxiliary_head=dict(
+        align_corners=True,
+        num_classes=66))
