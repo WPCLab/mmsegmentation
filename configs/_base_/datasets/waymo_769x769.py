851b9f7f56f7aa25d51c94df3b55a65f57d6912c
darrenwang <darrenwang@didiglobal.com>
2022-12-14 14:52:23 +0800

    modify waymo config

--- configs/_base_/datasets/waymo_769x769.py
+++ configs/_base_/datasets/waymo_769x769.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'WaymoDataset'
-data_root = '/nfs/volume-807-2/darrenwang/waymo'
+data_root = '/nfs/dataset-dtai-common/waymo_2d_segmentation'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size = (769, 769)
--- configs/_base_/datasets/waymo_896x896.py
+++ configs/_base_/datasets/waymo_896x896.py
@@ -1,9 +1,9 @@
 # dataset settings
 dataset_type = 'WaymoDataset'
-data_root = '/nfs/volume-807-2/darrenwang/waymo'
+data_root = '/nfs/dataset-dtai-common/waymo_2d_segmentation'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-crop_size = (640, 640)
+crop_size = (896, 896)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
